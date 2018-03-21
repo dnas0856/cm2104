c235eddde8e3d90e7cfd0c373b693e65a5af5ce6
dnas0856 <35962984+dnas0856@users.noreply.github.com>
2018-03-21 20:33:27 +0000

    Add files via upload

---
+++ js/prettyPrintJSON.js
@@ -0,0 +1,31 @@
+$(function(){
+  //ready
+  alert ("document ready");
+
+  $('#searchform').submit(function(){
+    var searchterms=$("#searchterms").val();
+    //call search youtube function
+    getResultsFromOMDB(searchterms);
+    return false;
+  });
+});
+
+function getResultsFromOMDB(searchterms){
+  //call api using ajax
+  //build url for the request
+  var url ="http://www.omdbapi.com/?apikey=1b4f6264&s=" + searchterms;
+  //use jquery json shortcut
+  $.getJson(url,function(jsondata){
+    //handle the results
+    prettyprintJSON(jsondata);
+  });
+
+
+}
+
+
+function prettyprintJSON(jsondata){
+  //prints the json to the screen
+  var pretty= JSON.stringify(jsondata, null,4);
+  $('#resultsbox').append("<pre>"+pretty + "</pre>");
+}
---
+++ js/ready.js
@@ -0,0 +1,4 @@
+$(function(){
+  //ready
+  alert ("document ready");
+});
